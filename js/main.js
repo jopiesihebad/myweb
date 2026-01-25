fffc72ff6b0e0df380b7210fca5572bf4240a8f0
jopiesihebad <159938698+jopiesihebad@users.noreply.github.com>
2026-01-25 08:35:55 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-  const x = (e.clientX / window.innerWidth - 0.5) * 25;
-  const y = (e.clientY / window.innerHeight - 0.5) * 25;
-
-  const mesh = document.querySelector(".mesh-bg");
-  if (mesh) {
-    mesh.style.transform = `translate(${x}px, ${y}px)`;
-  }
-});
